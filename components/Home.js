cac243cd6bca248cddeabcbaf2172edd3349e509
Valerie Ingmann <valerieingmann707@gmail.com>
2020-10-17 15:46:49 -0400

    feat: integrated firebase auth

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Text, View, Button } from "react-native";
+import { firebase } from "../firebase";
 
-export default function Home(props) {
+export default function Home() {
+	const signOut = () => {
+		firebase
+			.auth()
+			.signOut()
+			.then(() => {
+				console.log("User is signed out");
+			})
+			.catch(error => alert(error));
+	};
 	return (
 		<View>
 			<Text>Home</Text>
-			<Button title="Sign Out" />
+			<Button title="Sign Out" onPress={signOut} />
 		</View>
 	);
 }
