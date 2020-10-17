dae3ba137bb2dcb3cc8a010e967d4c9a6a9015a9
Valerie Ingmann <valerieingmann707@gmail.com>
2020-10-16 20:11:36 -0400

    feat: integrated firebase

--- App.js
+++ App.js
@@ -6,16 +6,41 @@ import Home from "./components/Home";
 import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
 
+import { View, Text } from "react-native";
+import { useAuth, userContext } from "./context";
+
 const Stack = createStackNavigator();
 
 export default function App() {
+	// listens for changes in authentication
+	const { initializing, user } = useAuth();
+
+	// renders a loading page if still initializing
+	if (initializing) {
+		return (
+			<View>
+				<Text>Loading</Text>
+			</View>
+		);
+	}
+
 	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="LogIn" component={LogIn} />
-				<Stack.Screen name="SignUp" component={SignUp} />
-				<Stack.Screen name="Home" component={Home} />
-			</Stack.Navigator>
-		</NavigationContainer>
+		// wrap navigation in the userContext.Provider
+		<userContext.Provider value={{ user }}>
+			<NavigationContainer>
+				<Stack.Navigator>
+					{user ? (
+						<>
+							<Stack.Screen name="Home" component={Home} />
+						</>
+					) : (
+						<>
+							<Stack.Screen name="LogIn" component={LogIn} />
+							<Stack.Screen name="SignUp" component={SignUp} />
+						</>
+					)}
+				</Stack.Navigator>
+			</NavigationContainer>
+		</userContext.Provider>
 	);
 }
